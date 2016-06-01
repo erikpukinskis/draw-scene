9e4525c69b6bc1e71c972e6d362a3350b0f5ba86
Erik Pukinskis <erik@snowedin.net>
2016-06-01 14:28:48 -0700

    make camera optional

--- draw-scene.js
+++ draw-scene.js
@@ -203,7 +203,9 @@ var drawScene = (function() {
   }
 
   function paint(shapes, cam) {
-    camera = cam
+    if (cam) {
+      camera = cam
+    }
 
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
